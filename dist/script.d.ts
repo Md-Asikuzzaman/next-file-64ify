447d7bdf6140f80d0fe77ebc3e032f4223d24fdc
Md-Asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-05-14 18:22:10 +0600

    update the package

--- dist/script.d.ts
+++ dist/script.d.ts
@@ -1,1 +1,6 @@
-export declare const base64: (file: File) => Promise<string | null>;
+export interface Base64Type {
+    data?: string | null;
+    isLoading: boolean;
+    isError: boolean;
+}
+export declare const _64ify: (file: File, fileType: string[]) => Promise<Base64Type>;
